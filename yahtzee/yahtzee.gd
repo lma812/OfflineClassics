a54e812839b9fb37f7389debd616c8b338fb426c
Edmond <edmondhl004@gmail.com>
2026-04-17 12:33:32 -0400

    Project SetUp + Basic Home

---
+++ yahtzee/yahtzee.gd
@@ -0,0 +1,13 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	$BacktoMenu.pressed.connect(_on_back_button_pressed)
+	
+func _on_back_button_pressed():
+	get_tree().change_scene_to_file("res://main_menu.tscn")
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
