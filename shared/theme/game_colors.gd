aaf63cba45de6124eca2765208265aff89a0c233
Edmond <edmondhl004@gmail.com>
2026-04-22 12:57:51 -0400

    colors and autoloaded

---
+++ shared/theme/game_colors.gd
@@ -0,0 +1,11 @@
+extends Node
+
+#colors so far
+#Added this script to project's autoloader so you can access these colors 
+#anywhere doing ex: GameColors.YALE_BLUE
+
+const YALE_BLUE = Color("#284b63ff") #current primary blue 
+const SOFT_LINEN = Color("#EEF0EB") #current primary white
+const DEEP_SPACE_BLUE = Color("#153243") 
+const IVORY = Color("#F4F9E9") 
+const ASH_GREY = Color("#B4B8AB") 
