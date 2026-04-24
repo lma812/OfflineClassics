7de09fa1ab634bd41ec1a13557f12b4b0f910981
Edmond <edmondhl004@gmail.com>
2026-04-24 10:10:43 -0400

    basic functional yahtzee home screen

--- yahtzee/yahtzee.gd
+++ yahtzee/yahtzee.gd
@@ -1,16 +1,45 @@
 extends Control
-#const COLORS = preload("res://shared/theme/colors.tres")
+
+var difficulties = ["Easy", "Medium", "Hard"]
+var selected_diff = "Medium"
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	#print(COLORS)
-	#print(COLORS.primary)
-	#$BacktoMenu.modulate = COLORS.primary
-	$BacktoMenu.pressed.connect(_on_back_button_pressed)
+	#var style = StyleBoxFlat.new()
+	#style.bg_color = GameColors.YALE_BLUE
+	#$Control/VBoxContainer/BackButton.add_theme_stylebox_override("normal", style)
 	
+	$StartScreen/Container/VBoxContainer/BackButton.pressed.connect(_on_back_button_pressed)
+	$StartScreen.visible = true
+	$StartScreen/Container/VBoxContainer/DifficultyValueLabel.text= "Medium"
+	$StartScreen/Container/VBoxContainer/DifficultyValueLabel.modulate = GameColors.YELLOW
+	
+	$StartScreen/Container/VBoxContainer/HSlider.value_changed.connect(_on_slider_changed)
+	$StartScreen/Container/VBoxContainer/PlayButton.pressed.connect(_on_play_pressed)
+	
+
+
+
+
 func _on_back_button_pressed():
 	get_tree().change_scene_to_file("res://main_menu.tscn")
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _on_slider_changed(value: float):
+	selected_diff = difficulties[int(value)]
+	$StartScreen/Container/VBoxContainer/DifficultyValueLabel.text = selected_diff
+	
+	#label color change per level
+	match selected_diff:
+		"Easy": $StartScreen/Container/VBoxContainer/DifficultyValueLabel.modulate = GameColors.SPOTIFY_GREEN
+		"Medium": $StartScreen/Container/VBoxContainer/DifficultyValueLabel.modulate = GameColors.YELLOW
+		"Hard": $StartScreen/Container/VBoxContainer/DifficultyValueLabel.modulate = GameColors.RED
+	
+
+func _on_play_pressed():
+	$StartScreen.visible = false
+	
+
+
+	# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
