6db5a26ae44c79f721f14b1812c3768bee27b32e
Edmond <edmondhl004@gmail.com>
2026-04-28 01:06:13 -0400

    res

--- main_menu.gd
+++ main_menu.gd
@@ -3,13 +3,13 @@ extends Control
 func _ready():
 	$CenterContainer/VBoxContainer/YahtzeeButton.pressed.connect(_on_yahtzee_pressed)
 	$CenterContainer/VBoxContainer/SnakeButton.pressed.connect(_on_snake_pressed)
-	#$CenterContainer/VBoxContainer/Game2048Button.pressed.connect(_on_2048_pressed)
+	$CenterContainer/VBoxContainer/Game2048Button.pressed.connect(_on_2048_pressed)
 
 func _on_yahtzee_pressed():
 	get_tree().change_scene_to_file("res://yahtzee/yahtzee.tscn")
 
 func _on_snake_pressed():
 	get_tree().change_scene_to_file("res://snake/components/ui/start_screen.tscn")
-#
-#func _on_2048_pressed():
-	#get_tree().change_scene_to_file("res://game_2048/game_2048.tscn")
+
+func _on_2048_pressed():
+	get_tree().change_scene_to_file("res://2048/2048.tscn")
