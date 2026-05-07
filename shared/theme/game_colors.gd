f97336b478cb9da78c25d9497f8fc6853a54cc54
lma812 <lawrencema812@gmail.com>
2026-05-07 17:13:50 -0400

    first version of theme file & test on main menu

--- shared/theme/game_colors.gd
+++ shared/theme/game_colors.gd
@@ -9,6 +9,7 @@ const SOFT_LINEN = Color("#EEF0EB") #current primary white
 const DEEP_SPACE_BLUE = Color("#153243") 
 const IVORY = Color("#F4F9E9") 
 const ASH_GREY = Color("#B4B8AB") 
+
 const SPOTIFY_GREEN = Color("#1DB954")
 const YELLOW = Color("#ebcc14")
 const RED = Color("#e31c1e")
