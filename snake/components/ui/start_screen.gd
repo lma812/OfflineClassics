01dcb515fb7a127242338cef7faee0cf75b445f5
lma812 <lawrencema812@gmail.com>
2026-05-07 17:44:03 -0400

    merge UI from snake start screen

--- snake/components/ui/start_screen.gd
+++ snake/components/ui/start_screen.gd
@@ -2,17 +2,13 @@ extends Control
 #const COLORS = preload("res://shared/theme/colors.tres")
 
 # Called when the node enters the scene tree for the first time.
-var snake = SaveManager.get_game("snake")
-var high_score = snake["high_score"]
-var games_played = snake["games_played"]
-
 func _ready():
 	#print(COLORS)
 	#print(COLORS.primary)
 	#$BacktoMenu.modulate = COLORS.primary
 	$Menu/BacktoMenu.pressed.connect(_on_back_button_pressed)
 	$Menu/StartGame.pressed.connect(_on_start_game_button_pressed)
-	$Hud.get_node("ScoreLabel").text = "HIGHSCORE: %s \t GAMES PLAYED: %s" % [high_score, games_played]
+	$Hud/ScoreLabel.hide()
 func _on_back_button_pressed():
 	get_tree().change_scene_to_file("res://main_menu.tscn")
 
