8ed3b7099acd80a8b94e7f3cfc4a72e21c943f14
lma812 <lawrence.ma16@bcmail.cuny.edu>
2026-05-08 01:43:45 -0400

    consistent theming

--- snake/components/ui/start_screen.gd
+++ snake/components/ui/start_screen.gd
@@ -2,13 +2,16 @@ extends Control
 #const COLORS = preload("res://shared/theme/colors.tres")
 
 # Called when the node enters the scene tree for the first time.
+var snake = SaveManager.get_game("snake")
+
 func _ready():
 	#print(COLORS)
 	#print(COLORS.primary)
 	#$BacktoMenu.modulate = COLORS.primary
 	$Menu/BacktoMenu.pressed.connect(_on_back_button_pressed)
 	$Menu/StartGame.pressed.connect(_on_start_game_button_pressed)
-	$Hud/ScoreLabel.hide()
+	$Hud.get_node("ScoreLabel").text = "HIGHSCORE: %s \t GAMES PLAYED: %s" % [int(snake["high_score"]), int(snake["games_played"])]	
+	
 func _on_back_button_pressed():
 	get_tree().change_scene_to_file("res://main_menu.tscn")
 
